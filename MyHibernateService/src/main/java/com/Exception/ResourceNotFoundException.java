6f48745f90c0639bdbb0d27af463fd16869a1e04
NitinSharma1991 <nitin.rebelee91@gmail.com>
2018-09-04 21:57:47 +0530

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: NitinSharma1991 <nitin.rebelee91@gmail.com>
    
    # Conflicts:
    #	.idea/workspace.xml

---
+++ MyHibernateService/src/main/java/com/Exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.Exception;
+
+public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(){}
+    public ResourceNotFoundException(String s){
+        super(s);
+    }
+
+}
